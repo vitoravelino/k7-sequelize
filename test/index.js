68c34d1aa73981e2d11d963db782cb7c1c54de01
Fernando Cagale <fernando.b.cagale@gmail.com>
2016-03-14 08:49:53 -0300

    Add test for object Sequelize

--- test/index.js
+++ test/index.js
@@ -57,6 +57,7 @@ describe('K7Sequelize', () => {
       expect(server).to.deep.include('database');
       expect(server.database).to.be.an.object();
       expect(server.database).to.deep.include('sequelize');
+      expect(server.database).to.deep.include('Sequelize');
       expect(server.database).to.deep.include('User');
       done();
     });
@@ -98,6 +99,7 @@ describe('K7Sequelize', () => {
       expect(server).to.deep.include('database');
       expect(server.database).to.be.an.object();
       expect(server.database).to.deep.include('sequelize');
+      expect(server.database).to.deep.include('Sequelize');
       expect(server.database).to.deep.include('User');
       done();
     });
@@ -141,6 +143,7 @@ describe('K7Sequelize', () => {
       expect(server).to.deep.include('database');
       expect(server.database).to.be.an.object();
       expect(server.database).to.deep.include('sequelize');
+      expect(server.database).to.deep.include('Sequelize');
       expect(server.database).to.deep.include('User');
       done();
     });
@@ -200,17 +203,24 @@ describe('K7Sequelize', () => {
         done();
       });
     });
+
+    it('should have object Sequelize', (done) => {
+      const k7 = new K7Sequelize();
+
+      expect(k7.db).to.contain('Sequelize');
+      done();
+    });
   });
 });
 
 function buildUri () {
-  let username = process.env.DB_USERNAME || 'root';
-  let password = process.env.DB_PASSWORD || null;
+  let username = process.env.DB_USERNAME || 'postgres';
+  let password = process.env.DB_PASSWORD || 'postgres';
   return url.format({
     protocol: 'postgresql',
     slashes: true,
     auth: username + ':' + password,
-    port: process.env.DB_PORT || 5432,
+    port: process.env.DB_PORT || 5434,
     hostname: process.env.DB_HOST || 'localhost',
     pathname: process.env.DB_NAME || 'project'
   });
